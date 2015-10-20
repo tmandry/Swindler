d55a9daf02b1c64f45046478d9e81c37817e4c08
Tyler Mandry <tmandry@gmail.com>
2015-10-19 23:57:25 -0500

    Create Swindler project

---
+++ Swindler/Swindler/Swindler.h
@@ -0,0 +1,19 @@
+//
+//  Swindler.h
+//  Swindler
+//
+//  Created by Tyler Mandry on 10/19/15.
+//  Copyright © 2015 Tyler Mandry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swindler.
+FOUNDATION_EXPORT double SwindlerVersionNumber;
+
+//! Project version string for Swindler.
+FOUNDATION_EXPORT const unsigned char SwindlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swindler/PublicHeader.h>
+
+
