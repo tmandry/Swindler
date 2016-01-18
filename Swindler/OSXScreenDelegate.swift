d3cf1d024ef65b2e526cb5d53a03de01223b15db
Tyler Mandry <tmandry@gmail.com>
2016-01-17 20:55:30 -0600

    Add useful debugDescription for Screens

--- Swindler/OSXScreenDelegate.swift
+++ Swindler/OSXScreenDelegate.swift
@@ -24,10 +24,63 @@ final class OSXScreenDelegate: ScreenDelegate {
     return other.directDisplayID == self.directDisplayID
   }
 
+  /// The name for the display (usually, the manufacturer and model number).
+  lazy var displayName: String = {
+    guard let info = infoForCGDisplay(self.directDisplayID, options: kIODisplayOnlyPreferredName) else {
+      return "Unknown screen"
+    }
+    guard let localizedNames = info[kDisplayProductName] as! NSDictionary? as Dictionary?,
+              name           = localizedNames.values.first as! NSString? as String? else {
+      return "Unnamed screen"
+    }
+    return name
+  }()
+
   var debugDescription: String {
-    return nsScreen.debugDescription
+    return "\"\(displayName)\" \(frame)"
   }
 
   var frame: CGRect { return nsScreen.frame }
   var applicationFrame: CGRect { return nsScreen.visibleFrame }
+}
+
+/// Returns the IODisplay info dictionary for the given displayID.
+///
+/// -Returns: The info dictionary for the first screen with the same vendor and model number as the
+///           specified screen.
+func infoForCGDisplay(displayID: CGDirectDisplayID, options: Int) -> [NSObject: AnyObject]? {
+  var iter: io_iterator_t = 0
+
+  // Initialize iterator.
+  let services = IOServiceMatching("IODisplayConnect")
+  let err = IOServiceGetMatchingServices(kIOMasterPortDefault, services, &iter)
+  guard err == KERN_SUCCESS else {
+    log.warn("Could not find services for IODisplayConnect, error code \(err)")
+    return nil
+  }
+
+  // Loop through all screens, looking for a vendor and model ID match.
+  for var service = IOIteratorNext(iter); service != 0; service = IOIteratorNext(iter) {
+    let info = IODisplayCreateInfoDictionary(service, IOOptionBits(options)).takeRetainedValue() as Dictionary
+
+    guard let cfVendorID  = info[kDisplayVendorID] as! CFNumber?,
+              cfProductID = info[kDisplayProductID] as! CFNumber? else {
+      log.warn("Missing vendor or product ID encountered when looping through screens")
+      continue
+    }
+
+    var vendorID: CFIndex = 0, productID: CFIndex = 0
+    guard CFNumberGetValue(cfVendorID, .CFIndexType, &vendorID) &&
+          CFNumberGetValue(cfProductID, .CFIndexType, &productID) else {
+      log.warn("Unexpected failure unwrapping vendor or product ID while looping through screens")
+      continue
+    }
+
+    if UInt32(vendorID) == CGDisplayVendorNumber(displayID) &&
+       UInt32(productID) == CGDisplayModelNumber(displayID) {
+      return info
+    }
+  }
+
+  return nil
 }
