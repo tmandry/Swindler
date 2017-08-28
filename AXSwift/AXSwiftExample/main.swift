c67ff438b8a2cdd4c0d2f24a948fff15295f7eac
Tyler Mandry <tmandry@gmail.com>
2017-08-27 19:30:22 -0500

    Remove AXSwift subtree

--- AXSwift/AXSwiftExample/main.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-
-let applicationDelegate = ApplicationDelegate()
-let application = NSApplication.shared()
-application.setActivationPolicy(NSApplicationActivationPolicy.accessory)
-application.delegate = applicationDelegate
-application.run()
--- AXSwift/AXSwiftObserverExample/main.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-
-let applicationDelegate = AppDelegate()
-let application = NSApplication.shared()
-application.setActivationPolicy(NSApplicationActivationPolicy.accessory)
-application.delegate = applicationDelegate
-application.run()
