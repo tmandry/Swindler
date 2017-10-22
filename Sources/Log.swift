f072804eb053d58ecf55e10f796c1a2a42001f8a
Tyler Mandry <tmandry@gmail.com>
2017-10-22 17:31:01 -0500

    Stop using ASL logging (deprecated anyway) to avoid header problems

--- Sources/Log.swift
+++ Sources/Log.swift
@@ -1,56 +1,71 @@
-import ASLLog
+//import ASLLog
 
 /// Internal logger.
 internal private(set) var log = Log()
 
 private var xcodeColorsEnabled = (ProcessInfo().environment["XcodeColors"] == "YES")
 
+struct StderrOutputStream: TextOutputStream {
+  public mutating func write(_ string: String) {
+    fputs(string, stderr)
+  }
+}
+var stderrStream = StderrOutputStream()
+
 /// Internal logging methods. Uses ASL to log to system console at correct log levels.
 struct Log {
     private static var __once: () = {
-        asl_add_log_file(nil, STDERR_FILENO)
-        asl_set_filter(nil, aslFilterMaskUpTo(ASL_LEVEL_DEBUG))
+        //asl_add_log_file(nil, STDERR_FILENO)
+        //asl_set_filter(nil, aslFilterMaskUpTo(ASL_LEVEL_DEBUG))
     }()
     static var token: Int = 0
     init() {
         _ = Log.__once
     }
 
+    enum Level {
+        case error
+        case warn
+        case notice
+        case info
+        case debug
+        case trace
+    }
+
     // TODO: filter out logs depending on build settings.
 
     /// Log that something has failed.
     func error(_ out: @autoclosure () -> String) {
-        log(out(), level: ASL_LEVEL_ERR, withColor: Color.red)
+        log(out(), level: .error, withColor: Color.red)
     }
-
     /// Log that something is amiss which might result in a failure.
     func warn(_ out: @autoclosure () -> String) {
-        log(out(), level: ASL_LEVEL_WARNING, withColor: Color.yellow)
+        log(out(), level: .warn, withColor: Color.yellow)
     }
 
     /// Log something of moderate interest to the user or administrator.
     func notice(_ out: @autoclosure () -> String) {
-        log(out(), level: ASL_LEVEL_NOTICE, withColor: Color.purple)
+        log(out(), level: .notice, withColor: Color.purple)
     }
 
     /// Log something purely informational (not visible in production).
     func info(_ out: @autoclosure () -> String) {
 #if SWINDLER_DEBUG
-        log(out(), level: ASL_LEVEL_INFO, withColor: Color.cyan)
+        log(out(), level: .info, withColor: Color.cyan)
 #endif
     }
 
     /// Log debug info (not visible in production).
     func debug(_ out: @autoclosure () -> String) {
 #if SWINDLER_DEBUG
-        log(out(), level: ASL_LEVEL_DEBUG, withColor: Color.blue)
+        log(out(), level: .debug, withColor: Color.blue)
 #endif
     }
 
     /// Log more verbose debug info (usually not visible in production or development).
     func trace(_ out: @autoclosure () -> String) {
 #if SWINDLER_TRACE
-        log(out(), level: ASL_LEVEL_DEBUG, withColor: Color.gray)
+        log(out(), level: .trace, withColor: Color.gray)
 #endif
     }
 
@@ -69,7 +84,7 @@ struct Log {
     }
 
     // Log on the given log level, using the given color if XcodeColors is enabled.
-    fileprivate func log(_ string: String, level: Int32, withColor: Color? = nil) {
+    fileprivate func log(_ string: String, level: Level, withColor: Color? = nil) {
         var output = ""
         if let color = withColor, xcodeColorsEnabled {
             let escape = "\u{001b}["
@@ -78,7 +93,8 @@ struct Log {
         } else {
             output = string
         }
-        aslLog(output, level)
+        //aslLog(output, level)
+        print(output, to: &stderrStream)
     }
 
 }
--- Swindler.podspec
+++ Swindler.podspec
@@ -31,11 +31,6 @@ Pod::Spec.new do |s|
   s.source       = { :git => 'https://github.com/tmandry/Swindler.git', :commit => 'c2b871e1d4f47b82b87510fed6ca320f97e3f2d0' }# :tag => s.version.to_s }
 
   s.source_files = 'Sources', 'Sources/**/*.{h,swift}'
-  s.public_header_files = 'Sources/ASLLog/ASLLog.h'
-
-  s.pod_target_xcconfig = {'SWIFT_INCLUDE_PATHS' => '$(SRCROOT)/Swindler/Sources/ASLLog/**',
-                           'LIBRARY_SEARCH_PATHS' => '$(SRCROOT)/Swindler/Sources/ASLLog'}
-  s.preserve_paths = 'Sources/ASLLog/module.modulemap'
 
   s.dependency 'PromiseKit', '4.4.0'
   s.dependency 'AXSwift', '0.2.0'
